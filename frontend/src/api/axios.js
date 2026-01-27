dce4b3e2f94230f56ea43b318f7e513051826a18
VincentLeduc <vincent.leduc@protonmail.com>
2026-01-27 11:52:30 +0100

    changement url serveur

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8080/api",
+  // baseURL: "http://127.0.0.1:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
   headers: {
     "Content-Type": "application/ld+json"
   }
