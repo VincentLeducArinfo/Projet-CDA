299c60c2f2014fee606ce73caad83e5addecd2db
VincentLeduc <vincent.leduc@protonmail.com>
2026-02-06 14:22:32 +0100

    nettoyage commentaire

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -9,27 +9,4 @@ const api = axios.create({
   }
 });
 
-// Intecepteur pour ajouter le token JWT
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token && (router.currentRoute.value.name != 'login' && router.currentRoute.value.name != 'register')) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-
-// Intercepter les requêtes pour vérifier la validité du token
-// api.interceptors.response.use(function onFulfilled(response) {
-//   // Any status code that lie within the range of 2xx cause this function to trigger
-//   // Do something with response data
-//   return response;
-// }, function onRejected(error) {
-//   if (error.response.status == 401) {
-//     router.push({ name: 'login' });
-//   }
-
-//   return Promise.reject(error);
-// });
-
-
-
 export default api;
